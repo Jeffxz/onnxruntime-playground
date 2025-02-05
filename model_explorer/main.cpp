dd730c1f50b3bd0b95b49f1306c9754933d51697
Zheng Xu <zhengxu7806@gmail.com>
2025-02-04 20:05:55 -0500

    add fns_candy example

--- model_explorer/main.cpp
+++ model_explorer/main.cpp
@@ -18,7 +18,7 @@
  *
  */
 
-#include <algorithm>  // std::generate
+#include <algorithm> // std::generate
 #include <cassert>
 #include <cstddef>
 #include <cstdint>
@@ -27,36 +27,42 @@
 #include <string>
 #include <vector>
 #include <onnxruntime_cxx_api.h>
+#include <QCoreApplication>
+#include <QDebug>
 
 // pretty prints a shape dimension vector
-std::string print_shape(const std::vector<std::int64_t>& v) {
+std::string print_shape(const std::vector<std::int64_t> &v)
+{
   std::stringstream ss("");
-  for (std::size_t i = 0; i < v.size() - 1; i++) ss << v[i] << "x";
+  for (std::size_t i = 0; i < v.size() - 1; i++)
+    ss << v[i] << "x";
   ss << v[v.size() - 1];
   return ss.str();
 }
 
-int calculate_product(const std::vector<std::int64_t>& v) {
+int calculate_product(const std::vector<std::int64_t> &v)
+{
   int total = 1;
-  for (auto& i : v) total *= i;
+  for (auto &i : v)
+    total *= i;
   return total;
 }
 
 template <typename T>
-Ort::Value vec_to_tensor(std::vector<T>& data, const std::vector<std::int64_t>& shape) {
+Ort::Value vec_to_tensor(std::vector<T> &data, const std::vector<std::int64_t> &shape)
+{
   Ort::MemoryInfo mem_info =
       Ort::MemoryInfo::CreateCpu(OrtAllocatorType::OrtArenaAllocator, OrtMemType::OrtMemTypeDefault);
   auto tensor = Ort::Value::CreateTensor<T>(mem_info, data.data(), data.size(), shape.data(), shape.size());
   return tensor;
 }
 
-#ifdef _WIN32
-int wmain(int argc, ORTCHAR_T* argv[]) {
-#else
-int main(int argc, ORTCHAR_T* argv[]) {
-#endif
-  if (argc != 2) {
-    std::cout << "Usage: ./onnx-api-example <onnx_model.onnx>" << std::endl;
+int main(int argc, ORTCHAR_T *argv[])
+{
+  QCoreApplication app(argc, argv);
+  if (argc < 2)
+  {
+    qDebug() << "Usage: " << argv[0] << "<onnx_model.onnx>";
     return -1;
   }
 
@@ -71,26 +77,30 @@ int main(int argc, ORTCHAR_T* argv[]) {
   Ort::AllocatorWithDefaultOptions allocator;
   std::vector<std::string> input_names;
   std::vector<std::int64_t> input_shapes;
-  std::cout << "Input Node Name/Shape (" << input_names.size() << "):" << std::endl;
-  for (std::size_t i = 0; i < session.GetInputCount(); i++) {
+  qDebug() << "Input Node Name/Shape (" << input_names.size() << "):";
+  for (std::size_t i = 0; i < session.GetInputCount(); i++)
+  {
     input_names.emplace_back(session.GetInputNameAllocated(i, allocator).get());
     input_shapes = session.GetInputTypeInfo(i).GetTensorTypeAndShapeInfo().GetShape();
-    std::cout << "\t" << input_names.at(i) << " : " << print_shape(input_shapes) << std::endl;
+    qDebug() << "\t" << input_names.at(i) << " : " << print_shape(input_shapes);
   }
   // some models might have negative shape values to indicate dynamic shape, e.g., for variable batch size.
-  for (auto& s : input_shapes) {
-    if (s < 0) {
+  for (auto &s : input_shapes)
+  {
+    if (s < 0)
+    {
       s = 1;
     }
   }
 
   // print name/shape of outputs
   std::vector<std::string> output_names;
-  std::cout << "Output Node Name/Shape (" << output_names.size() << "):" << std::endl;
-  for (std::size_t i = 0; i < session.GetOutputCount(); i++) {
+  qDebug() << "Output Node Name/Shape (" << output_names.size() << "):";
+  for (std::size_t i = 0; i < session.GetOutputCount(); i++)
+  {
     output_names.emplace_back(session.GetOutputNameAllocated(i, allocator).get());
     auto output_shapes = session.GetOutputTypeInfo(i).GetTensorTypeAndShapeInfo().GetShape();
-    std::cout << "\t" << output_names.at(i) << " : " << print_shape(output_shapes) << std::endl;
+    qDebug() << "\t" << output_names.at(i) << " : " << print_shape(output_shapes);
   }
 
   // Assume model has 1 input node and 1 output node.
@@ -102,34 +112,40 @@ int main(int argc, ORTCHAR_T* argv[]) {
 
   // generate random numbers in the range [0, 255]
   std::vector<float> input_tensor_values(total_number_elements);
-  std::generate(input_tensor_values.begin(), input_tensor_values.end(), [&] { return rand() % 255; });
+  std::generate(input_tensor_values.begin(), input_tensor_values.end(), [&]
+                { return rand() % 255; });
   std::vector<Ort::Value> input_tensors;
   input_tensors.emplace_back(vec_to_tensor<float>(input_tensor_values, input_shape));
 
   // double-check the dimensions of the input tensor
   assert(input_tensors[0].IsTensor() && input_tensors[0].GetTensorTypeAndShapeInfo().GetShape() == input_shape);
-  std::cout << "\ninput_tensor shape: " << print_shape(input_tensors[0].GetTensorTypeAndShapeInfo().GetShape()) << std::endl;
+  qDebug() << "\ninput_tensor shape: " << print_shape(input_tensors[0].GetTensorTypeAndShapeInfo().GetShape());
 
   // pass data through model
-  std::vector<const char*> input_names_char(input_names.size(), nullptr);
+  std::vector<const char *> input_names_char(input_names.size(), nullptr);
   std::transform(std::begin(input_names), std::end(input_names), std::begin(input_names_char),
-                 [&](const std::string& str) { return str.c_str(); });
+                 [&](const std::string &str)
+                 { return str.c_str(); });
 
-  std::vector<const char*> output_names_char(output_names.size(), nullptr);
+  std::vector<const char *> output_names_char(output_names.size(), nullptr);
   std::transform(std::begin(output_names), std::end(output_names), std::begin(output_names_char),
-                 [&](const std::string& str) { return str.c_str(); });
+                 [&](const std::string &str)
+                 { return str.c_str(); });
 
-  std::cout << "Running model..." << std::endl;
-  try {
+  qDebug() << "Running model...";
+  try
+  {
     auto output_tensors = session.Run(Ort::RunOptions{nullptr}, input_names_char.data(), input_tensors.data(),
                                       input_names_char.size(), output_names_char.data(), output_names_char.size());
-    std::cout << "Done!" << std::endl;
+    qDebug() << "Done!";
 
     // double-check the dimensions of the output tensors
     // NOTE: the number of output tensors is equal to the number of output nodes specifed in the Run() call
     assert(output_tensors.size() == output_names.size() && output_tensors[0].IsTensor());
-  } catch (const Ort::Exception& exception) {
-      std::cout << "ERROR running model inference: " << exception.what() << std::endl;
+  }
+  catch (const Ort::Exception &exception)
+  {
+    qDebug() << "ERROR running model inference: " << exception.what();
     exit(-1);
   }
 }
